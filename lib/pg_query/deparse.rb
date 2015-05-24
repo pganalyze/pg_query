0d98e8d3c48235ec077c8fa8859d398854bc7952
Lukas Fittl <lukas@fittl.com>
2015-05-24 12:35:03 -0700

    Add deparsing support for complex SELECTs.

--- lib/pg_query/deparse.rb
+++ lib/pg_query/deparse.rb
@@ -24,6 +24,8 @@ class PgQuery
       deparse_columnref(node)
     when 'A_CONST'
       deparse_a_const(node)
+    when 'A_STAR'
+      deparse_a_star(node)
     when 'ALIAS'
       deparse_alias(node)
     when 'PARAMREF'
@@ -32,6 +34,8 @@ class PgQuery
       deparse_restarget(node, context)
     when 'FUNCCALL'
       deparse_funccall(node)
+    when 'RANGEFUNCTION'
+      deparse_range_function(node)
     when 'AEXPR AND'
       deparse_aexpr_and(node)
     when 'JOINEXPR'
@@ -40,10 +44,18 @@ class PgQuery
       deparse_sortby(node)
     when 'SELECT'
       deparse_select(node)
+    when 'WITHCLAUSE'
+      deparse_with_clause(node)
+    when 'COMMONTABLEEXPR'
+      deparse_cte(node)
     when 'INSERT INTO'
       deparse_insert_into(node)
     when 'UPDATE'
       deparse_update(node)
+    when 'TYPECAST'
+      deparse_typecast(node)
+    when 'TYPENAME'
+      deparse_typename(node)
     else
       fail format("Can't deparse: %s: %s", type, node.inspect)
     end
@@ -57,11 +69,17 @@ class PgQuery
   end
 
   def deparse_columnref(node)
-    node['fields'].join('.')
+    node['fields'].map do |field|
+      field.is_a?(String) ? field : deparse_item(field)
+    end.join('.')
   end
 
   def deparse_a_const(node)
     node['val'].inspect.gsub('"', '\'')
+  end
+
+  def deparse_a_star(_node)
+    '*'
   end
 
   def deparse_alias(node)
@@ -85,8 +103,16 @@ class PgQuery
   end
 
   def deparse_funccall(node)
-    args = node['args'].map { |arg| deparse_item(arg) }
+    args = Array(node['args']).map { |arg| deparse_item(arg) }
     format('%s(%s)', node['funcname'].join('.'), args.join(', '))
+  end
+
+  def deparse_range_function(node)
+    output = []
+    output << 'LATERAL' if node['lateral']
+    output << deparse_item(node['functions'][0][0]) # FIXME: Needs more test cases
+    output << deparse_item(node['alias']) if node['alias']
+    output.join(' ')
   end
 
   def deparse_aexpr(node)
@@ -103,6 +129,7 @@ class PgQuery
   def deparse_joinexpr(node)
     output = []
     output << deparse_item(node['larg'])
+    output << 'LEFT' if node['jointype'] == 1
     output << 'JOIN'
     output << deparse_item(node['rarg'])
 
@@ -121,8 +148,35 @@ class PgQuery
     output.join(' ')
   end
 
-  def deparse_select(node)
+  def deparse_with_clause(node)
+    output = ['WITH']
+    output << 'RECURSIVE' if node['recursive']
+    output << node['ctes'].map do |cte|
+      deparse_item(cte)
+    end.join(', ')
+    output.join(' ')
+  end
+
+  def deparse_cte(node)
+    output = ''
+    output += node['ctename']
+    output += format('(%s)', node['aliascolnames'].join(', ')) if node['aliascolnames']
+    output += format(' AS (%s)', deparse_item(node['ctequery']))
+    output
+  end
+
+  def deparse_select(node) # rubocop:disable Metrics/CyclomaticComplexity
     output = []
+
+    if node['op'] == 1
+      output << deparse_item(node['larg'])
+      output << 'UNION'
+      output << 'ALL' if node['all']
+      output << deparse_item(node['rarg'])
+      return output.join(' ')
+    end
+
+    output << deparse_item(node['withClause']) if node['withClause']
 
     if node['targetList']
       output << 'SELECT'
@@ -133,9 +187,9 @@ class PgQuery
 
     if node['fromClause']
       output << 'FROM'
-      node['fromClause'].each do |item|
-        output << deparse_item(item)
-      end
+      output << node['fromClause'].map do |item|
+        deparse_item(item)
+      end.join(', ')
     end
 
     if node['whereClause']
@@ -190,5 +244,21 @@ class PgQuery
     end
 
     output.join(' ')
+  end
+
+  def deparse_typecast(node)
+    if deparse_item(node['typeName']) == :boolean
+      deparse_item(node['arg']) == "'t'" ? 'true' : 'false'
+    else
+      fail format("Can't deparse typecast %s", node.inspect)
+    end
+  end
+
+  def deparse_typename(node)
+    if node['names'] == ['pg_catalog', 'bool']
+      :boolean
+    else
+      nil
+    end
   end
 end
--- spec/lib/deparse_spec.rb
+++ spec/lib/deparse_spec.rb
@@ -1,16 +1,63 @@
 require 'spec_helper'
 
 describe PgQuery, 'deparse' do
-  subject { PgQuery.parse(query).deparse }
+  subject { PgQuery.parse(oneline_query).deparse }
 
-  context 'basic SELECT statements' do
-    let(:query) { 'SELECT a AS b FROM x WHERE y = 5 AND z = y' }
-    it { is_expected.to eq query }
-  end
+  let (:oneline_query) { query.gsub(/\s+/, ' ').gsub('( ', '(').gsub(' )', ')').strip }
 
-  context 'complex SELECT statements' do
-    let(:query) { "SELECT memory_total_bytes, memory_swap_total_bytes - memory_swap_free_bytes AS swap, date_part($0, s.collected_at) AS collected_at FROM snapshots s JOIN system_snapshots ON snapshot_id = s.id WHERE s.database_id = $0 AND s.collected_at >= $0 AND s.collected_at <= $0 ORDER BY collected_at ASC" }
-    it { is_expected.to eq query }
+  context 'SELECT' do
+    context 'basic statement' do
+      let(:query) { 'SELECT a AS b FROM x WHERE y = 5 AND z = y' }
+      it { is_expected.to eq query }
+    end
+
+    context 'complex SELECT statement' do
+      let(:query) { "SELECT memory_total_bytes, memory_swap_total_bytes - memory_swap_free_bytes AS swap, date_part($0, s.collected_at) AS collected_at FROM snapshots s JOIN system_snapshots ON snapshot_id = s.id WHERE s.database_id = $0 AND s.collected_at >= $0 AND s.collected_at <= $0 ORDER BY collected_at ASC" }
+      it { is_expected.to eq query }
+    end
+
+    context 'simple WITH statement' do
+      let(:query) { 'WITH t AS (SELECT random() AS x FROM generate_series(1, 3)) SELECT * FROM t' }
+      it { is_expected.to eq query }
+    end
+
+    context 'complex WITH statement' do
+      let(:query) {
+        """
+        WITH RECURSIVE employee_recursive(distance, employee_name, manager_name) AS (
+          SELECT 1, employee_name, manager_name
+          FROM employee
+          WHERE manager_name = 'Mary'
+        UNION ALL
+          SELECT er.distance + 1, e.employee_name, e.manager_name
+          FROM employee_recursive er, employee e
+          WHERE er.employee_name = e.manager_name
+        )
+        SELECT distance, employee_name FROM employee_recursive
+        """
+      }
+      it { is_expected.to eq oneline_query }
+    end
+
+    context 'LATERAL' do
+      let(:query) { 'SELECT m.name AS mname, pname FROM manufacturers m, LATERAL get_product_names(m.id) pname' }
+      it { is_expected.to eq query }
+    end
+
+    context 'LATERAL JOIN' do
+      let(:query) {
+        """
+        SELECT m.name AS mname, pname
+          FROM manufacturers m LEFT JOIN LATERAL get_product_names(m.id) pname ON true
+        """
+      }
+      it { is_expected.to eq oneline_query }
+    end
+
+    context 'omitted FROM clause' do
+      let(:query) { 'SELECT 2 + 2' }
+      it { is_expected.to eq query }
+    end
   end
 
   context 'basic INSERT statements' do
