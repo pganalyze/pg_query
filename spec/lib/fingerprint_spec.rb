90892065c99f4fee671e3717e403a95fab258c7d
Lukas Fittl <lukas@fittl.com>
2015-12-13 04:00:45 -0800

    Add more fingerprint examples.

--- spec/lib/fingerprint_spec.rb
+++ spec/lib/fingerprint_spec.rb
@@ -28,6 +28,13 @@ describe PgQuery, "#fingerprint" do
   it "returns expected hash values" do
     expect(fingerprint('SELECT 1')).to eq '31dc5500dc27777a26160cb1b0faa11495f150d8'
     expect(fingerprint('SELECT COUNT(DISTINCT id), * FROM targets WHERE something IS NOT NULL AND elsewhere::interval < now()')).to eq '1e014ccea580bb5dea8b4a66893b3c508d6261f0'
+    expect(fingerprint('INSERT INTO test (a, b) VALUES (?, ?)')).to eq 'f3f2847a56d9b67f11e1905d2365bc627f852220'
+    expect(fingerprint('INSERT INTO test (b, a) VALUES (?, ?)')).to eq 'f3f2847a56d9b67f11e1905d2365bc627f852220'
+    expect(fingerprint('SELECT b AS x, a AS y FROM z')).to eq '7c361dd7a746418464fdf666cfae7be6a0f873aa'
+    expect(fingerprint('SELECT * FROM x WHERE y IN (?)')).to eq 'd15431e54000f340c2bfca70ed3a0f31b2e55061'
+    expect(fingerprint('SELECT * FROM x WHERE y IN (?, ?, ?)')).to eq 'd15431e54000f340c2bfca70ed3a0f31b2e55061'
+    expect(fingerprint('SELECT * FROM x WHERE y IN ( ?::uuid )')).to eq 'bd7dcab89d5a8ad04b5f7e352030f47d5abd1eab'
+    expect(fingerprint('SELECT * FROM x WHERE y IN ( ?::uuid, ?::uuid, ?::uuid )')).to eq 'bd7dcab89d5a8ad04b5f7e352030f47d5abd1eab'
   end
 
   it "returns expected hash parts" do
@@ -39,6 +46,27 @@ describe PgQuery, "#fingerprint" do
       "something", "false", "1", "A_Expr", "0", "TypeCast", "ColumnRef", "String", "elsewhere",
       "TypeName", "String", "pg_catalog", "String", "interval", "false", "false", "0", "-1",
       "String", "<", "FuncCall", "false", "false", "false", "false", "String", "now"
+    ])
+    expect(fingerprint_parts('INSERT INTO test (a, b) VALUES (?, ?)')).to eq([
+      "InsertStmt", "ResTarget", "a", "ResTarget", "b", "RangeVar", "2", "test", "p", "SelectStmt", "false", "0"
+    ])
+    expect(fingerprint_parts('INSERT INTO test (b, a) VALUES (?, ?)')).to eq([
+      "InsertStmt", "ResTarget", "a", "ResTarget", "b", "RangeVar", "2", "test", "p", "SelectStmt", "false", "0"
+    ])
+    expect(fingerprint_parts('SELECT b AS x, a AS y FROM z')).to eq([
+      "SelectStmt", "false", "RangeVar", "2", "z", "p", "0", "ResTarget", "ColumnRef", "String", "a", "ResTarget", "ColumnRef", "String", "b"
+    ])
+    expect(fingerprint_parts('SELECT * FROM x WHERE y IN (?)')).to eq([
+      "SelectStmt", "false", "RangeVar", "2", "x", "p", "0", "ResTarget", "ColumnRef", "A_Star", "A_Expr", "9", "ColumnRef", "String", "y", "String", "="
+    ])
+    expect(fingerprint_parts('SELECT * FROM x WHERE y IN (?, ?, ?)')).to eq([
+      "SelectStmt", "false", "RangeVar", "2", "x", "p", "0", "ResTarget", "ColumnRef", "A_Star", "A_Expr", "9", "ColumnRef", "String", "y", "String", "="
+    ])
+    expect(fingerprint_parts('SELECT * FROM x WHERE y IN ( ?::uuid )')).to eq([
+      "SelectStmt", "false", "RangeVar", "2", "x", "p", "0", "ResTarget", "ColumnRef", "A_Star", "A_Expr", "9", "ColumnRef", "String", "y", "String", "=", "TypeCast", "TypeName", "String", "uuid", "false", "false", "0", "-1"
+    ])
+    expect(fingerprint_parts('SELECT * FROM x WHERE y IN ( ?::uuid, ?::uuid, ?::uuid )')).to eq([
+      "SelectStmt", "false", "RangeVar", "2", "x", "p", "0", "ResTarget", "ColumnRef", "A_Star", "A_Expr", "9", "ColumnRef", "String", "y", "String", "=", "TypeCast", "TypeName", "String", "uuid", "false", "false", "0", "-1"
     ])
   end
 
