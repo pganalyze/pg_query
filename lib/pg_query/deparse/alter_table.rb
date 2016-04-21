4ed01136dd54e2fff40db564f8948d4a261ec403
Lukas Fittl <lukas@fittl.com>
2016-04-20 20:14:00 -0700

    Add support for Ruby 1.9, and test Rubinius on Travis.

--- lib/pg_query/deparse/alter_table.rb
+++ lib/pg_query/deparse/alter_table.rb
@@ -20,22 +20,22 @@ class PgQuery
       end
 
       ALTER_TABLE_TYPES_MAPPING = {
-        AT_AddColumn                 => -> (_node) { ['ADD COLUMN'] },
-        AT_ColumnDefault             => -> (node) { ['ALTER COLUMN', node['def'] ? 'SET DEFAULT' : 'DROP DEFAULT'] },
-        AT_DropNotNull               => -> (_node) { ['ALTER COLUMN', 'DROP NOT NULL'] },
-        AT_SetNotNull                => -> (_node) { ['ALTER COLUMN', 'SET NOT NULL'] },
-        AT_SetStatistics             => -> (_node) { ['ALTER COLUMN', 'SET STATISTICS'] },
-        AT_SetOptions                => -> (_node) { ['ALTER COLUMN', 'SET'] },
-        AT_ResetOptions              => -> (_node) { ['ALTER COLUMN', 'RESET'] },
-        AT_SetStorage                => -> (_node) { ['ALTER COLUMN', 'SET STORAGE'] },
-        AT_DropColumn                => -> (_node) { ['DROP'] },
-        AT_AddIndex                  => -> (_node) { ['ADD INDEX'] },
-        AT_AddConstraint             => -> (_node) { ['ADD'] },
-        AT_AlterConstraint           => -> (_node) { ['ALTER CONSTRAINT'] },
-        AT_ValidateConstraint        => -> (_node) { ['VALIDATE CONSTRAINT'] },
-        AT_DropConstraint            => -> (_node) { ['DROP CONSTRAINT'] },
-        AT_AlterColumnType           => -> (_node) { ['ALTER COLUMN', 'TYPE'] },
-        AT_AlterColumnGenericOptions => -> (_node) { ['ALTER COLUMN', 'OPTIONS'] }
+        AT_AddColumn                 => ->(_node) { ['ADD COLUMN'] },
+        AT_ColumnDefault             => ->(node) { ['ALTER COLUMN', node['def'] ? 'SET DEFAULT' : 'DROP DEFAULT'] },
+        AT_DropNotNull               => ->(_node) { ['ALTER COLUMN', 'DROP NOT NULL'] },
+        AT_SetNotNull                => ->(_node) { ['ALTER COLUMN', 'SET NOT NULL'] },
+        AT_SetStatistics             => ->(_node) { ['ALTER COLUMN', 'SET STATISTICS'] },
+        AT_SetOptions                => ->(_node) { ['ALTER COLUMN', 'SET'] },
+        AT_ResetOptions              => ->(_node) { ['ALTER COLUMN', 'RESET'] },
+        AT_SetStorage                => ->(_node) { ['ALTER COLUMN', 'SET STORAGE'] },
+        AT_DropColumn                => ->(_node) { ['DROP'] },
+        AT_AddIndex                  => ->(_node) { ['ADD INDEX'] },
+        AT_AddConstraint             => ->(_node) { ['ADD'] },
+        AT_AlterConstraint           => ->(_node) { ['ALTER CONSTRAINT'] },
+        AT_ValidateConstraint        => ->(_node) { ['VALIDATE CONSTRAINT'] },
+        AT_DropConstraint            => ->(_node) { ['DROP CONSTRAINT'] },
+        AT_AlterColumnType           => ->(_node) { ['ALTER COLUMN', 'TYPE'] },
+        AT_AlterColumnGenericOptions => ->(_node) { ['ALTER COLUMN', 'OPTIONS'] }
       }.freeze
     end
   end
