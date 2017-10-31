de2c9d1f61d7be1d2ba8be24bc495221cbc626a1
Lukas Fittl <lukas@fittl.com>
2017-10-31 02:16:12 -0700

    Update libpg_query

--- ext/pg_query/extconf.rb
+++ ext/pg_query/extconf.rb
@@ -3,7 +3,7 @@
 require 'mkmf'
 require 'open-uri'
 
-LIB_PG_QUERY_TAG = '10-1.0.0'.freeze
+LIB_PG_QUERY_TAG = '10-1.0.1'.freeze
 
 workdir = Dir.pwd
 libdir = File.join(workdir, 'libpg_query-' + LIB_PG_QUERY_TAG)
