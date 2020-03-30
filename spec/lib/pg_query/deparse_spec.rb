ef6ab67bce602c164fdddde8eb479c9a234ae212
Mehmet Emin KARAKAŞ <mkarakas@turksat.com.tr>
2020-03-30 15:19:51 +0300

    LATERAL JOIN problem
    expected: SELECT *
                                FROM "tb_test_main" mh
                                JOIN LATERAL (
                                  SELECT "ftnrm".* FROM "test" ftnrm WHERE "ftnrm"."hizmet_id" = "mh"."id"
                                  UNION ALL
                                  SELECT "ftarc".* FROM "test"."test2" ftarc WHERE "ftarc"."hizmet_id" = "mh"."id"
                                ) ft ON true
                        )
    
    got: SELECT *
                           FROM "tb_test_main" mh
                           JOIN (
                             SELECT "ftnrm".* FROM "test" ftnrm WHERE "ftnrm"."hizmet_id" = "mh"."id"
                             UNION ALL
                             SELECT "ftarc".* FROM "test"."test2" ftarc WHERE "ftarc"."hizmet_id" = "mh"."id"
                           ) ft ON true
                   )

--- spec/lib/pg_query/deparse_spec.rb
+++ spec/lib/pg_query/deparse_spec.rb
@@ -169,22 +169,6 @@ describe PgQuery::Deparse do
         it { is_expected.to eq query }
       end
 
-      context 'LATERAL JOIN 2' do
-        let(:query) do
-          %(
-          SELECT *
-                  FROM "tb_test_main" mh
-                  JOIN LATERAL (
-                    SELECT "ftnrm".* FROM "test" ftnrm WHERE "ftnrm"."hizmet_id" = "mh"."id"
-                    UNION ALL
-                    SELECT "ftarc".* FROM "test"."test2" ftarc WHERE "ftarc"."hizmet_id" = "mh"."id"
-                  ) ft ON true
-          )
-        end
-
-        it { is_expected.to eq oneline_query }
-      end
-
       context 'LATERAL' do
         let(:query) { 'SELECT "m"."name" AS mname, "pname" FROM "manufacturers" m, LATERAL get_product_names("m"."id") pname' }
 
@@ -196,6 +180,22 @@ describe PgQuery::Deparse do
           %(
           SELECT "m"."name" AS mname, "pname"
             FROM "manufacturers" m LEFT JOIN LATERAL get_product_names("m"."id") pname ON true
+          )
+        end
+
+        it { is_expected.to eq oneline_query }
+      end
+
+      context 'LATERAL JOIN with sql' do
+        let(:query) do
+          %(
+          SELECT *
+                  FROM "tb_test_main" mh
+                  JOIN LATERAL (
+                    SELECT "ftnrm".* FROM "test" ftnrm WHERE "ftnrm"."hizmet_id" = "mh"."id"
+                    UNION ALL
+                    SELECT "ftarc".* FROM "test"."test2" ftarc WHERE "ftarc"."hizmet_id" = "mh"."id"
+                  ) ft ON true
           )
         end
 
