e271d8cebfc8b8ac99359c48da54d1f09d750ef3
Lukas Fittl <lukas@fittl.com>
2021-02-28 16:41:10 -0800

    Replace on-demand libpg_query source download with bundled source code
    Its unnecessary to download the source on-demand, and makes this more
    complex than it needs to be. Instead, introduce a new "update_source" rake
    task that can be called to refresh the source for a specified revision.

--- Rakefile
+++ Rakefile
@@ -2,6 +2,10 @@ require 'bundler/gem_tasks'
 require 'rake/extensiontask'
 require 'rspec/core/rake_task'
 require 'rubocop/rake_task'
+require 'open-uri'
+
+LIB_PG_QUERY_TAG = '13-latest-develop'.freeze
+LIB_PG_QUERY_SHA256SUM = 'FIXFORRELEASE'.freeze
 
 Rake::ExtensionTask.new 'pg_query' do |ext|
   ext.lib_dir = 'lib/pg_query'
@@ -20,4 +24,63 @@ task :clean do
   FileUtils.rm_rf File.join(__dir__, 'tmp/')
   FileUtils.rm_f Dir.glob(File.join(__dir__, 'ext/pg_query/*.o'))
   FileUtils.rm_f File.join(__dir__, 'lib/pg_query/pg_query.bundle')
+end
+
+task :update_source do
+  workdir = File.join(__dir__, 'tmp')
+  libdir = File.join(workdir, 'libpg_query-' + LIB_PG_QUERY_TAG)
+  filename = File.join(workdir, 'libpg_query-' + LIB_PG_QUERY_TAG + '.tar.gz')
+  testfilesdir = File.join(__dir__, 'spec/files')
+  extdir = File.join(__dir__, 'ext/pg_query')
+  extbakdir = File.join(workdir, 'extbak')
+
+  unless File.exist?(filename)
+    File.open(filename, 'wb') do |target_file|
+      URI.open('https://codeload.github.com/lfittl/libpg_query/tar.gz/' + LIB_PG_QUERY_TAG, 'rb') do |read_file|
+        target_file.write(read_file.read)
+      end
+    end
+
+    checksum = Digest::SHA256.hexdigest(File.read(filename))
+
+    #if checksum != LIB_PG_QUERY_SHA256SUM
+    #  raise "SHA256 of #{filename} does not match: got #{checksum}, expected #{expected_sha256}"
+    #end
+  end
+
+  unless Dir.exist?(libdir)
+    system("tar -xzf #{filename} -C #{workdir}") || raise('ERROR')
+  end
+
+  # Backup important files from ext dir
+  system("rm -fr #{extbakdir}")
+  system("mkdir #{extbakdir}")
+  system("cp -a #{extdir}/pg_query_ruby.{c,sym} #{extdir}/extconf.rb #{extbakdir}")
+
+  FileUtils.rm_rf extdir
+
+  # Reduce everything down to one directory
+  system("mkdir #{extdir}")
+  system("cp -a #{libdir}/src/* #{extdir}/")
+  system("mv #{extdir}/postgres/* #{extdir}/")
+  system("rmdir #{extdir}/postgres")
+  system("cp -a #{libdir}/pg_query.h #{extdir}/include")
+  # Make sure every .c file in the top-level directory is its own translation unit
+  system("mv #{extdir}/*{_conds,_defs,_helper}.c #{extdir}/include")
+  # Protobuf definitions
+  #system("cp -a #{libdir}/protobuf/parse_tree.proto nodes/")
+  #system("PATH=$(PWD)/bin:$(PATH) protoc --go_out=. nodes/parse_tree.proto")
+  system("protoc --proto_path=#{libdir}/protobuf --ruby_out=#{File.join(__dir__, 'lib/pg_query')} #{libdir}/protobuf/pg_query.proto")
+  # Protobuf library code
+  system("mkdir -p #{extdir}/include/protobuf")
+  system("cp -a #{libdir}/protobuf/*.h #{extdir}/include/protobuf")
+  system("mkdir -p #{extdir}/include/protobuf-c")
+  system("cp -a #{libdir}/protobuf-c/*.h #{extdir}/include/protobuf-c")
+  system("cp -a #{libdir}/protobuf-c/*.h #{extdir}/include")
+  system("cp -a #{libdir}/protobuf/*.c #{extdir}/")
+  system("cp -a #{libdir}/protobuf-c/*.c #{extdir}/")
+  # Other support files
+  system("cp -a #{libdir}/testdata/* #{testfilesdir}")
+  # Copy back the custom ext files
+  system("cp -a #{extbakdir}/pg_query_ruby.{c,sym} #{extbakdir}/extconf.rb #{extdir}")
 end
--- ext/pg_query/pg_query_ruby.c
+++ ext/pg_query/pg_query_ruby.c
@@ -1,11 +1,13 @@
-#include "pg_query_ruby.h"
+#include "pg_query.h"
+#include <ruby.h>
 
-void raise_ruby_parse_error(PgQueryParseResult result);
+void raise_ruby_parse_error(PgQueryProtobufParseResult result);
 void raise_ruby_normalize_error(PgQueryNormalizeResult result);
 void raise_ruby_fingerprint_error(PgQueryFingerprintResult result);
 void raise_ruby_scan_error(PgQueryScanResult result);
 
-VALUE pg_query_ruby_parse(VALUE self, VALUE input);
+VALUE pg_query_ruby_parse_protobuf(VALUE self, VALUE input);
+VALUE pg_query_ruby_deparse_protobuf(VALUE self, VALUE input);
 VALUE pg_query_ruby_normalize(VALUE self, VALUE input);
 VALUE pg_query_ruby_fingerprint(VALUE self, VALUE input);
 VALUE pg_query_ruby_scan(VALUE self, VALUE input);
@@ -16,13 +18,14 @@ void Init_pg_query(void)
 
 	cPgQuery = rb_const_get(rb_cObject, rb_intern("PgQuery"));
 
-	rb_define_singleton_method(cPgQuery, "_raw_parse", pg_query_ruby_parse, 1);
+	rb_define_singleton_method(cPgQuery, "parse_protobuf", pg_query_ruby_parse_protobuf, 1);
+	rb_define_singleton_method(cPgQuery, "deparse_protobuf", pg_query_ruby_deparse_protobuf, 1);
 	rb_define_singleton_method(cPgQuery, "normalize", pg_query_ruby_normalize, 1);
 	rb_define_singleton_method(cPgQuery, "fingerprint", pg_query_ruby_fingerprint, 1);
 	rb_define_singleton_method(cPgQuery, "_raw_scan", pg_query_ruby_scan, 1);
 }
 
-void raise_ruby_parse_error(PgQueryParseResult result)
+void raise_ruby_parse_error(PgQueryProtobufParseResult result)
 {
 	VALUE cPgQuery, cParseError;
 	VALUE args[4];
@@ -35,7 +38,25 @@ void raise_ruby_parse_error(PgQueryParseResult result)
 	args[2] = INT2NUM(result.error->lineno);
 	args[3] = INT2NUM(result.error->cursorpos);
 
-	pg_query_free_parse_result(result);
+	pg_query_free_protobuf_parse_result(result);
+
+	rb_exc_raise(rb_class_new_instance(4, args, cParseError));
+}
+
+void raise_ruby_deparse_error(PgQueryDeparseResult result)
+{
+	VALUE cPgQuery, cParseError;
+	VALUE args[4];
+
+	cPgQuery    = rb_const_get(rb_cObject, rb_intern("PgQuery"));
+	cParseError = rb_const_get_at(cPgQuery, rb_intern("ParseError"));
+
+	args[0] = rb_str_new2(result.error->message);
+	args[1] = rb_str_new2(result.error->filename);
+	args[2] = INT2NUM(result.error->lineno);
+	args[3] = INT2NUM(result.error->cursorpos);
+
+	pg_query_free_deparse_result(result);
 
 	rb_exc_raise(rb_class_new_instance(4, args, cParseError));
 }
@@ -94,21 +115,42 @@ void raise_ruby_scan_error(PgQueryScanResult result)
 	rb_exc_raise(rb_class_new_instance(4, args, cScanError));
 }
 
-VALUE pg_query_ruby_parse(VALUE self, VALUE input)
+VALUE pg_query_ruby_parse_protobuf(VALUE self, VALUE input)
 {
 	Check_Type(input, T_STRING);
 
 	VALUE output;
-	PgQueryParseResult result = pg_query_parse(StringValueCStr(input));
+	PgQueryProtobufParseResult result = pg_query_parse_protobuf(StringValueCStr(input));
 
 	if (result.error) raise_ruby_parse_error(result);
 
 	output = rb_ary_new();
 
-	rb_ary_push(output, rb_str_new2(result.parse_tree));
+	rb_ary_push(output, rb_str_new(result.parse_tree.data, result.parse_tree.len));
 	rb_ary_push(output, rb_str_new2(result.stderr_buffer));
 
-	pg_query_free_parse_result(result);
+	pg_query_free_protobuf_parse_result(result);
+
+	return output;
+}
+
+VALUE pg_query_ruby_deparse_protobuf(VALUE self, VALUE input)
+{
+	Check_Type(input, T_STRING);
+
+	VALUE output;
+	PgQueryProtobuf pbuf = {0};
+	PgQueryDeparseResult result = {0};
+
+	pbuf.data = StringValuePtr(input);
+	pbuf.len = RSTRING_LEN(input);
+	result = pg_query_deparse_protobuf(pbuf);
+
+	if (result.error) raise_ruby_deparse_error(result);
+
+	output = rb_str_new2(result.query);
+
+	pg_query_free_deparse_result(result);
 
 	return output;
 }
@@ -160,7 +202,7 @@ VALUE pg_query_ruby_scan(VALUE self, VALUE input)
 
 	output = rb_ary_new();
 
-	rb_ary_push(output, rb_str_new(result.pbuf, result.pbuf_len));
+	rb_ary_push(output, rb_str_new(result.pbuf.data, result.pbuf.len));
 	rb_ary_push(output, rb_str_new2(result.stderr_buffer));
 
 	pg_query_free_scan_result(result);
