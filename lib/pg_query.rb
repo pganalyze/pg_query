ed5f9804508aeceb1d72ae2524bb6e77deb29017
Lukas Fittl <lukas@fittl.com>
2021-02-28 16:44:22 -0800

    Use actual Ruby classes for parser result, instead of decoded JSON
    This is essentialy to enable easy and fast two-way communication with
    the C library, and as a bonus makes for a better interaction on the Ruby
    side, as we are handling actual objects instead of hashes and arrays.
    
    Note this also unifies the scan output into the same protobuf file,
    although the scan result retains a different top-level protobuf
    message (ScanResult).

--- lib/pg_query.rb
+++ lib/pg_query.rb
@@ -1,15 +1,14 @@
 require 'pg_query/version'
 require 'pg_query/parse_error'
 
-require 'pg_query/parse_tree_pb'
+require 'pg_query/pg_query_pb'
 
 require 'pg_query/pg_query'
+require 'pg_query/constants'
 require 'pg_query/parse'
 require 'pg_query/treewalker'
 require 'pg_query/node_types'
 require 'pg_query/deep_dup'
-
-require 'pg_query/legacy_parsetree'
 
 require 'pg_query/filter_columns'
 require 'pg_query/fingerprint'
