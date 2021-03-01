d07e16389d67bf009060d9e3e8a7e0e78c57343b
Lukas Fittl <lukas@fittl.com>
2021-02-28 18:47:06 -0800

    Move PgQuery::Node convenience functions to own file, add #from helper
    PgQuery::Node.from(x) is short hand for
    PgQuery::Node.new(x_field_name => x), e.g. turning
    
    PgQuery::Node.new(select_stmt: PgQuery::SelectStmt.new) to
    PgQuery::Node.from(PgQuery::SelectStmt.new)

---
+++ lib/pg_query/node.rb
@@ -0,0 +1,29 @@
+module PgQuery
+  # Patch the auto-generated generic node type with additional convenience functions
+  class Node
+    def inspect
+      node ? format('<PgQuery::Node: %s: %s>', node, public_send(node).inspect) : '<PgQuery::Node>'
+    end
+
+    # Make it easier to initialize nodes from a given node child object
+    def self.from(node_field_val)
+      PgQuery::Node.new(node_field_name(node_field_val) => node_field_val)
+    end
+
+    private
+
+    # This needs to match libpg_query naming for the Node message field names
+    # (see "underscore" method in libpg_query's scripts/generate_protobuf_and_funcs.rb)
+    def self.node_field_name(node_field_val)
+      camel_cased_word = node_field_val.class.name.split('::').last
+      return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
+      word = camel_cased_word.to_s.gsub("::", "/")
+      word.gsub!(/^([A-Z\d])([A-Z][a-z])/, '\1__\2')
+      word.gsub!(/([A-Z\d]+[a-z]+)([A-Z][a-z])/, '\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word
+    end
+  end
+end
