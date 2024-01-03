b9fbe2c94140f27a1d0547dd59202927932672d6
Lukas Fittl <lukas@fittl.com>
2024-01-03 12:53:43 -0800

    PgQuery::Node: Add inner and inner= helpers to modify inner object
    Because of how oneof messages work in protobuf, its a bit tedious to
    make modifications to a Node object, in particular when changing the type
    of the inner object held within the Node in a generic code path (i.e.
    with a dynamic inner type), which required first knowing the name of the
    new inner object type, and then using public_send to set the new value.
    
    To help, introduce the new "inner" and "inner=" methods for PgQuery::Node,
    that get/set the inner object directly, avoiding the use of public_send.
    
    This will be of particular help for anyone utilizing the walk! API to
    make modifications to the query tree. To illustrate, an example is added
    to the treewalker spec showing how to utilize this.

--- lib/pg_query/node.rb
+++ lib/pg_query/node.rb
@@ -1,22 +1,27 @@
 module PgQuery
   # Patch the auto-generated generic node type with additional convenience functions
   class Node
+    def self.inner_class_to_name(klass)
+      @inner_class_to_name ||= descriptor.lookup_oneof('node').to_h { |f| [f.subtype.msgclass, f.name.to_sym] }
+      @inner_class_to_name[klass]
+    end
+
+    def inner
+      self[node.to_s]
+    end
+
+    def inner=(submsg)
+      name = self.class.inner_class_to_name(submsg.class)
+      public_send("#{name}=", submsg)
+    end
+
     def inspect
-      node ? format('<PgQuery::Node: %s: %s>', node, public_send(node).inspect) : '<PgQuery::Node>'
+      node ? format('<PgQuery::Node: %s: %s>', node, inner.inspect) : '<PgQuery::Node>'
     end
 
     # Make it easier to initialize nodes from a given node child object
     def self.from(node_field_val)
-      # This needs to match libpg_query naming for the Node message field names
-      # (see "underscore" method in libpg_query's scripts/generate_protobuf_and_funcs.rb)
-      node_field_name = node_field_val.class.name.split('::').last
-      node_field_name.gsub!(/^([A-Z\d])([A-Z][a-z])/, '\1__\2')
-      node_field_name.gsub!(/([A-Z\d]+[a-z]+)([A-Z][a-z])/, '\1_\2')
-      node_field_name.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-      node_field_name.tr!('-', '_')
-      node_field_name.downcase!
-
-      PgQuery::Node.new(node_field_name => node_field_val)
+      PgQuery::Node.new(inner_class_to_name(node_field_val.class) => node_field_val)
     end
 
     # Make it easier to initialize value nodes
