bb93934933369d2d7ef7557a13edaf586a844317
msepga <msepga2@gmail.com>
2024-11-26 16:56:33 -0500

    Fix tests

--- spec/lib/pg_query/deparse_bad_tree_spec.rb
+++ spec/lib/pg_query/deparse_bad_tree_spec.rb
@@ -25,7 +25,7 @@ describe PgQuery do
 
         expect { PgQuery.deparse(tree) }.to raise_error do |error|
           expect(error).to be_a(described_class::ParseError)
-          expect(error.message).to eq "deparse: error in deparseTargetList: ResTarget without val (postgres_deparse.c:1553)"
+          expect(error.message).to eq "deparse: error in deparseTargetList: ResTarget without val (postgres_deparse.c:1582)"
         end
       end
     end
