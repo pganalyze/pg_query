7eb8e4791333fa1cca8e36c4d3cf5a6fdf713364
PikachuEXE <git@pikachuexe.net>
2024-12-19 09:44:01 +0800

    Fix ruby 3.4 compatibility

--- ext/pg_query/extconf.rb
+++ ext/pg_query/extconf.rb
@@ -21,12 +21,36 @@ if RUBY_PLATFORM =~ /mswin/
   $INCFLAGS = "-I#{File.join(__dir__, 'include', 'postgres', 'port', 'win32_msvc')} " + $INCFLAGS
 end
 
-SYMFILE =
-  if RUBY_PLATFORM =~ /freebsd/
-    File.join(__dir__, 'pg_query_ruby_freebsd.sym')
-  elsif RUBY_PLATFORM !~ /cygwin|mswin|mingw|bccwin|wince|emx/
-    File.join(__dir__, 'pg_query_ruby.sym')
+def have_ruby_abi_version()
+  # ruby_abi_version is only available in development versions: https://github.com/ruby/ruby/pull/6231
+  return false if RUBY_PATCHLEVEL >= 0
+
+  m = /(\d+)\.(\d+)/.match(RUBY_VERSION)
+  if m.nil?
+    puts "Failed to parse ruby version: #{RUBY_VERSION}. Assuming ruby_abi_version symbol is NOT present."
+    return false
   end
+  major = m[1].to_i
+  minor = m[2].to_i
+  if major >= 3 and minor >= 2
+    puts "Ruby version #{RUBY_VERSION} >= 3.2. Assuming ruby_abi_version symbol is present."
+    return true
+  end
+  puts "Ruby version #{RUBY_VERSION} < 3.2. Assuming ruby_abi_version symbol is NOT present."
+  false
+end
+
+def ext_export_filename()
+  name = if RUBY_PLATFORM =~ /freebsd/
+    'pg_query_ruby_freebsd'
+  elsif RUBY_PLATFORM !~ /cygwin|mswin|mingw|bccwin|wince|emx/
+    'pg_query_ruby'
+  end
+  name += '-with-ruby-abi-version' if have_ruby_abi_version()
+  "#{name}.sym"
+end
+
+SYMFILE = File.join(__dir__, ext_export_filename())
 
 if RUBY_PLATFORM =~ /darwin/
   $DLDFLAGS << " -Wl,-exported_symbols_list #{SYMFILE}" unless defined?(::Rubinius)
