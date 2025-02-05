1b042f5432e266443c225fefb5974a8419220000
Lukas Fittl <lukas@fittl.com>
2025-02-04 22:29:12 -0800

    Simplify extconf.rb changes, rename symbol files for clarity

--- ext/pg_query/extconf.rb
+++ ext/pg_query/extconf.rb
@@ -21,36 +21,25 @@ if RUBY_PLATFORM =~ /mswin/
   $INCFLAGS = "-I#{File.join(__dir__, 'include', 'postgres', 'port', 'win32_msvc')} " + $INCFLAGS
 end
 
-def have_ruby_abi_version()
-  # ruby_abi_version is only available in development versions: https://github.com/ruby/ruby/pull/6231
-  return false if RUBY_PATCHLEVEL >= 0
-
+# "ruby_abi_version" is a required symbol to be exported on Ruby 3.2+ development releases
+# See https://github.com/ruby/ruby/pull/5474 and https://github.com/ruby/ruby/pull/6231
+def export_ruby_abi_version
+  return false if RUBY_PATCHLEVEL >= 0 # Not a development release
   m = /(\d+)\.(\d+)/.match(RUBY_VERSION)
-  if m.nil?
-    puts "Failed to parse ruby version: #{RUBY_VERSION}. Assuming ruby_abi_version symbol is NOT present."
-    return false
-  end
+  return false if m.nil?
   major = m[1].to_i
   minor = m[2].to_i
-  if major >= 3 and minor >= 2
-    puts "Ruby version #{RUBY_VERSION} >= 3.2. Assuming ruby_abi_version symbol is present."
-    return true
-  end
-  puts "Ruby version #{RUBY_VERSION} < 3.2. Assuming ruby_abi_version symbol is NOT present."
-  false
+  major >= 3 && minor >= 2
 end
 
-def ext_export_filename()
-  name = if RUBY_PLATFORM =~ /freebsd/
-    'pg_query_ruby_freebsd'
-  elsif RUBY_PLATFORM !~ /cygwin|mswin|mingw|bccwin|wince|emx/
-    'pg_query_ruby'
-  end
-  name += '-with-ruby-abi-version' if have_ruby_abi_version()
+def ext_symbols_filename
+  name = 'ext_symbols'
+  name += '_freebsd' if RUBY_PLATFORM =~ /freebsd/
+  name += '_with_ruby_abi_version' if export_ruby_abi_version
   "#{name}.sym"
 end
 
-SYMFILE = File.join(__dir__, ext_export_filename())
+SYMFILE = File.join(__dir__, ext_symbols_filename)
 
 if RUBY_PLATFORM =~ /darwin/
   $DLDFLAGS << " -Wl,-exported_symbols_list #{SYMFILE}" unless defined?(::Rubinius)
